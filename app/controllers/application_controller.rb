a4515c8cb30c296ce13b99298f4cd083203f233a
Sarah Schaller <sarcsch@aol.com>
2017-10-23 23:20:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,45 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get 'tweets/new' do
+    erb :'/tweets/create_tweet'
+  end
+
+  post '/tweets' do
+  end
+
+  get '/tweets/:id' do
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+  end
+
+  post '/tweets/:id/delete' do
+  end
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  helpers do
+    def current_user
+    end
+
+    def logged_in?
+    end
+  end
+
+
 end
