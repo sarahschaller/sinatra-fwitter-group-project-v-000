4e34585ae7cd63ef7ec0327d8bd73d13689a80b2
Sarah Schaller <sarcsch@aol.com>
2017-10-24 00:50:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -28,7 +30,7 @@ class ApplicationController < Sinatra::Base
   patch '/tweets/:id' do
   end
 
-  post '/tweets/:id/delete' do
+  delete '/tweets/:id/delete' do
   end
 
   get '/signup' do
